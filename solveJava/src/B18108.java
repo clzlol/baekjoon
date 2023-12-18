76a078fd186d6694fc22922003b7e91ef92f1cbb
clzlol <nyc0527@naver.com>
2023-12-18 14:56:50 +0900

    solve 18108

---
+++ solveJava/src/B18108.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class B18108 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int y = s.nextInt();
+		s.close();
+		System.out.println(y-543);
+	}
+}
