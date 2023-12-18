04297bfc8c9a77a37a5e786f7352eee5cfeb71c4
clzlol <nyc0527@naver.com>
2023-12-18 15:13:47 +0900

    solve 11382

---
+++ solveJava/src/B11382.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class B11382 {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    Long a = s.nextLong();
+    Long b = s.nextLong();
+    Long c = s.nextLong();
+    System.out.println(a+b+c);
+  }
+}
